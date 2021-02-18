ff4c8286d99d77b5798b4e083a31700b88ec78fe
AndreiGR <andryigr@gmail.com>
2021-02-19 01:35:41 +0300

    add html and styles

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var http = require('http');
-//var fs = require('fs');
-//const mime = require('mime');
+
 var extract = require('./extract');
 var read = require('./read');
 var wss = require('./websockets-server');
@@ -19,22 +18,8 @@ var server = http.createServer(function(req, res) {
 
 
   var filePath = extract(req.url);
-  //var fileType = mime.getType(filePath);
+
   var file = read(filePath,res);
-
-  /*fs.readFile(filePath, function(err, data) {
-    if (err) {
-      fs.readFile('app/err404.html', function(err, data) {
-        res.end(data);
-      });
-      return;
-    } else {
-      console.log('The file type is: ' + fileType);
-      res.setHeader('Content-Type', fileType);
-      res.end(data);
-    }
-
-  });*/
 
 });
 server.listen(3000);
